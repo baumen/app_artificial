5aa49c26fe5692973df66dd839d709efe5895079
jjesusmonroy <ppchuyjjms@gmail.com>
2018-09-04 12:52:16 -0700

    changes
    too godo

--- app/src/main/java/com/baumen/app_artificial/MainActivity.java
+++ app/src/main/java/com/baumen/app_artificial/MainActivity.java
@@ -2,12 +2,20 @@ package com.baumen.app_artificial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mensaje;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializar();
+        mensaje.setText("sicorrio");
+    }
+    private void inicializar(){
+        mensaje=(TextView)findViewById(R.id.mensaje);
+
     }
 }
